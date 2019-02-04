661a494961f8e5b3978c3f1b191f25efb9e50d7d
Cris Stanza <crisstanza@work>
2019-02-04 17:05:26 -0200

    Rota sem usar arquivo externo.

--- js/js.js
+++ js/js.js
@@ -14,13 +14,15 @@
 		app.controller('RepeatController', function($scope) {
 			$scope.examples = [
 				{ tag: '', title: 'Exemplo 1', description: 'ng-repeat, ng-if' },
-				{ tag: 'ex-2', title: 'Exemplo 2', description: 'ngRoute' }
+				{ tag: 'ex-2', title: 'Exemplo 2', description: 'ngRoute (templateUrl)' },
+				{ tag: 'ex-3', title: 'Exemplo 3', description: 'ngRoute (template)' }
 			];
 		});
 		app.config(function($routeProvider) {
 			$routeProvider
 				.when('/', { templateUrl : 'main.html' })
 				.when('/ex-2', { templateUrl : 'ex-2-rota.html' })
+				.when('/ex-3', { template : '<p>Rota sem usar arquivo externo!</p><br /><a href="#!">...voltar</a>' })
 			;
 		});
 	}
